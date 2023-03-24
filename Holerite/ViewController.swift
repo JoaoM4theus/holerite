87598028c84a9ce6cde588038f396e0f52190781
Joao Matheus <joao.rocha@corebiz.ag>
2023-03-24 20:25:46 -0300

    Initial Commit

---
+++ Holerite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Holerite
+//
+//  Created by Joao Matheus on 24/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
