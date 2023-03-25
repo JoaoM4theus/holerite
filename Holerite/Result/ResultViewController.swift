0e6556d818a36af83e5ad4cd0b704b711f19f658
Joao Matheus <joao.rocha@corebiz.ag>
2023-03-25 10:39:47 -0300

    Setting constraint and implementing table view data source.

--- Holerite/Result/ResultViewController.swift
+++ Holerite/Result/ResultViewController.swift
@@ -29,7 +29,43 @@ class ResultViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        setUpConstraint()
+    }
+
+    @objc func close() {
+        print("close")
+    }
+    
+    private func setUpConstraint() {
+        view.addSubview(closeButton)
+        view.addSubview(tableView)
+        
+        [
+            closeButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 22),
+            closeButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 22),
+            closeButton.widthAnchor.constraint(equalToConstant: 70),
+            closeButton.heightAnchor.constraint(equalToConstant: 18),
+            
+            tableView.topAnchor.constraint(equalTo: closeButton.bottomAnchor, constant: 22),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ].forEach { constraint in
+            constraint.isActive = true
+        }
+    }
+}
+
+extension ResultViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Testando"
+        return cell
     }
 
 }
