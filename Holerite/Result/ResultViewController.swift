2eb79647fe9ace3c26c30af54fb490259c6c2af6
Joao Matheus <joao.rocha@corebiz.ag>
2023-03-25 10:39:21 -0300

    Creating components necessaries to building `ResultViewController`

--- Holerite/Result/ResultViewController.swift
+++ Holerite/Result/ResultViewController.swift
@@ -9,6 +9,23 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    lazy var closeButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .clear
+        button.setTitle("FECHAR", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(close), for: .touchUpInside)
+        return button
+    }()
+
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .white
+        tableView.dataSource = self
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
